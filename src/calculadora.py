0bc4568a49145838adb4691bd08a50674d50b79c
Thomas <x@gmail.com>
2024-08-20 19:30:11 -0500

    Potencia y módulo
    Se adiciona la operación de potencia y módulo.

--- src/calculadora.py
+++ src/calculadora.py
@@ -13,6 +13,12 @@ def calcular(numero1, numero2, operacion):
         case '/':
             # Operación división
             respuesta = numero1 / numero2
+        case '^':
+            # Operación potencia
+            respuesta = numero1 ** numero2
+        case '%':
+            # Operación módulo
+            respuesta = numero1 % numero2
         case _:
             raise ValueError("Operación inválida.")
 
